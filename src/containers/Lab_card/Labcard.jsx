6b4a5d13c7859065090c415deb6a13cf940902c4
HagarSallam <hagersaid974@gmail.com>
2023-03-07 21:16:24 +0200

    feat: labs-page

---
+++ src/containers/Lab_card/Labcard.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./labcard.css";
+import Labs from "../../components/Labs/Labs";
+import bruteforce from "../../assets/bruteforce.png";
+import xss from "../../assets/xss.png";
+import csrf from "../../assets/csrf.png";
+import sql from "../../assets/sqlinjection.png";
+import weakid from "../../assets/id.png";
+import file from "../../assets/fileupload.png";
+const Labcard = () => (
+  <div className="bigbox">
+    <div className="wrapper">
+      <Labs img={sql} title="Sql Injection" />
+      <Labs img={xss} title="XSS" />
+      <Labs img={csrf} title="CSRF" />
+      <Labs img={bruteforce} title="Brute Force" />
+      <Labs img={weakid} title="Weak ID" />
+      <Labs img={file} title="File Upload" />
+      <Labs img={sql} title="Sql Injection" />
+      <Labs img={xss} title="XSS" />
+      <Labs img={csrf} title="CSRF" />
+      <Labs img={bruteforce} title="Brute Force" />
+      <Labs img={weakid} title="Weak ID" />
+      <Labs img={file} title="File Upload" />
+    </div>
+  </div>
+);
+export default Labcard;
